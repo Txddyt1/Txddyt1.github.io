d056671b29148008944f93a4a7893cdce062b56c
unknown <ti@FLE209D076415.alunos.sc.senac.br>
2025-08-04 20:44:31 -0300

    H3

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My First page in GitHub Pages - Igor Schuller</h2>
+      <h3>Um texto legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
